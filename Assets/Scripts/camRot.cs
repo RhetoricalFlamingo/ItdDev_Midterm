37e7c405d7d59836bc28cc046bf625a2ad126e0a
MC <mcmailbox014@gmail.com>
2018-10-18 11:46:19 -0400

    Gameplay complete, need final art pass

---
+++ Assets/Scripts/camRot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class camRot : MonoBehaviour
+{
+
+	public float maxRot;
+	public bool goingRight = true;
+
+	private float i = 2;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!goingRight)
+		{
+			this.transform.eulerAngles += new Vector3(0, 0, Time.deltaTime);
+
+			i += Time.deltaTime;
+			
+			if (i > maxRot)
+			{
+				goingRight = true;
+				i = 0;
+			}
+		}
+		else
+		{
+			this.transform.eulerAngles -= new Vector3(0, 0, Time.deltaTime);
+
+			i += Time.deltaTime;
+			
+			if (i > maxRot)
+			{
+				goingRight = false;
+				i = 0;
+			}
+		}
+	}
+}
--- Assets/Scripts/plateScript.cs
+++ Assets/Scripts/plateScript.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class plateScript : MonoBehaviour
 {
 
 	public GameObject resultsScreen;
+	public GameObject finalScore;
+	public GameObject controlsPrompt;
+	public GameObject playerPhone;
+	public GameObject regPoints;
+	
 	float i = 0;
+
+	public GameObject pan;
 	
 	// Use this for initialization
 	void Start () {
@@ -27,6 +35,16 @@ public class plateScript : MonoBehaviour
 		if (i >= 1.5f)
 		{
 			resultsScreen.SetActive(true);
+			finalScore.SetActive(true);
+			controlsPrompt.SetActive(false);
+			playerPhone.SetActive(false);
+			regPoints.SetActive(false);
+
+			pan.GetComponent<foodScript>().panSpeedX = 0;
+			pan.GetComponent<foodScript>().panSpeedY = 0;
+			
+			finalScore.GetComponent<Text>().text = pan.GetComponent<foodScript>().pointsMax + " Points";
+			
 			Cursor.lockState = CursorLockMode.None;
 			Cursor.visible = true;
 		}
